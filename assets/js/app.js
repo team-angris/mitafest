dc640c5cb6aa5e49fdca82e954b8ca29157ca094
tharunoptimus <tharunoptimus@outlook.com>
2023-02-15 01:36:48 +0530

    Added Countdown

---
+++ assets/js/app.js
@@ -0,0 +1,41 @@
+let daysLeft = document.getElementById("daysLeft")
+let hoursLeft = document.getElementById("hoursLeft")
+let minutesLeft = document.getElementById("minutesLeft")
+let secondsLeft = document.getElementById("secondsLeft")
+
+
+
+function updateTime() {
+    let now = new Date()
+    let eventDate = new Date(2023, 2, 1) // 1st March 2023
+    let currentTime = now.getTime()
+    let eventTime = eventDate.getTime()
+    let remTime = eventTime - currentTime
+
+    let s = Math.floor(remTime / 1000)
+    let m = Math.floor(s / 60)
+    let h = Math.floor(m / 60)
+    let d = Math.floor(h / 24)
+
+    h %= 24
+    m %= 60
+    s %= 60
+
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m
+    s = (s < 10) ? "0" + s : s
+
+    daysLeft.textContent = d
+    hoursLeft.textContent = h
+    minutesLeft.textContent = m
+    secondsLeft.textContent = s
+
+    setTimeout(updateTime, 1000)
+}
+
+
+async function main() {
+    updateTime()
+}
+
+main()
