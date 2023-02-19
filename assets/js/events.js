c924884a83a9386333a2aa513beef484fde0d502
tharunoptimus <tharunoptimus@outlook.com>
2023-02-20 05:29:54 +0530

    Added jsdoc for the event

--- assets/js/events.js
+++ assets/js/events.js
@@ -1,1 +1,39 @@
+// @ts-check
 
+let friday = "March 3 2022"
+let saturday = "March 4 2022"
+
+/** 
+ * @typedef {object} subEventOrganizers
+ * @property {string} subEventOrganizerName
+ * @property {string} subEventOrganizerContact
+ */
+
+
+/**
+* @typedef OrganizedEvent
+* @property {string} subEventName
+* @property {string} subEventCode
+* @property {string} subEventDesc
+* @property {string} subEventDuration
+* @property {string[]} subEventRules
+* @property {string} subEventDate
+* @property {string} subEventTime
+* @property {string} subEventVenue
+* @property {string} subEventTeamSize
+* @property {subEventOrganizers[]} subEventOrganizers
+*/
+
+
+/**
+* @typedef MitafestEvent
+* @property {string} eventCode
+* @property {string} eventName
+* @property {string} eventTagName
+* @property {OrganizedEvent[]} organizedEvents
+*/
+
+/**
+ * @type {MitafestEvent[]}
+ * @description This is the array of all the events
+ */
