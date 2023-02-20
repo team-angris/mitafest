15470662b504fc6946b827ef67332cbc16e59fd7
tharunoptimus <tharunoptimus@outlook.com>
2023-02-20 05:31:19 +0530

    Gets stuff from url params

--- assets/js/eventsHandler.js
+++ assets/js/eventsHandler.js
@@ -1,1 +1,25 @@
+let contentHeading = document.querySelector(".contentHeading")
+let contentSubHeading = document.querySelector(".contentSubHeading")
+let organizedEventsHeadingContainer = document.querySelector(
+	".organizedEventsHeadingContainer"
+)
+let organizedEventsContainer = document.querySelector(
+	".organizedEventsContainer"
+)
+
+let urlParams = new URLSearchParams(window.location.search)
+let eventCode = urlParams.get("eventCode")
+
+if (!eventCode) {
+	window.location.href = "/"
+}
+
+let mitafestEvent = events.find((event) => event.eventCode === eventCode)
+
+if (!mitafestEvent) {
+	window.location.href = "/"
+}
+
+contentHeading.innerHTML = mitafestEvent.eventName
+contentSubHeading.innerHTML = mitafestEvent.eventTagName
 
