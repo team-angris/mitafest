fc3d1f302b9bb9e64e54466caaff649dd4930ab2
tharunoptimus <tharunoptimus@outlook.com>
2023-02-22 22:33:23 +0530

    Added core events data

---
+++ assets/js/publicEvents.js
@@ -0,0 +1,234 @@
+
+let publicEventsThursday = [
+    {
+        eventName: "Inaguration",
+        eventTimingsStart: "05:00 PM",
+        eventTimingsEnd: "06:00 PM",
+        eventLocation: "Rajam Hall"
+    },
+    {
+        eventName: "Dinner",
+        eventTimingsStart: "06:00 PM",
+        eventTimingsEnd: "06:30 PM",
+        eventLocation: ""
+    },
+    {
+        eventName: "Vibez Intra",
+        eventTimingsStart: "06:45 PM",
+        eventTimingsEnd: "08:00 PM",
+        eventLocation: "Open Air Theatre"
+    },
+    {
+        eventName: "Variety Intra",
+        eventTimingsStart: "08:00 PM",
+        eventTimingsEnd: "08:30 PM",
+        eventLocation: "Open Air Theatre"
+    },
+    {
+        eventName: "Dance Intra",
+        eventTimingsStart: "08:30 PM",
+        eventTimingsEnd: "09:00 PM",
+        eventLocation: "Open Air Theatre"
+    },
+    {
+        eventName: "Celebrities & Concerts",
+        eventTimingsStart: "09:00 PM",
+        eventTimingsEnd: "10:00 PM",
+        eventLocation: "Open Air Theatre"
+    }
+]
+
+let publicEventsFriday = [
+    {
+        eventName: "Solo Dance Intra",
+        eventTimingsStart: "09:00 AM",
+        eventTimingsEnd: "10:30 AM",
+        eventLocation: "Rajam Hall"
+    },
+    {
+        eventName: "Solo Singing Inter",
+        eventTimingsStart: "10:30 AM",
+        eventTimingsEnd: "11:30 AM",
+        eventLocation: "Rajam Hall"
+    },
+    {
+        eventName: "Duo Dance Intra",
+        eventTimingsStart: "11:30 AM",
+        eventTimingsEnd: "12:30 PM",
+        eventLocation: "Rajam Hall"
+    },
+    {
+        eventName: "Lunch",
+        eventTimingsStart: "12:30 PM",
+        eventTimingsEnd: "01:30 PM",
+        eventLocation: ""
+    },
+    {
+        eventName: "Solo Intrumental Inter",
+        eventTimingsStart: "01:30 PM",
+        eventTimingsEnd: "02:15 PM",
+        eventLocation: "Rajam Hall"
+    },
+    {
+        eventName: "Adzap Intra",
+        eventTimingsStart: "02:15 PM",
+        eventTimingsEnd: "03:00 PM",
+        eventLocation: "Rajam Hall"   
+    },
+    {
+        eventName: "Adaptunes Intra",
+        eventTimingsStart: "03:00 PM",
+        eventTimingsEnd: "04:00 PM",
+        eventLocation: "Rajam Hall"
+    },
+    {
+        eventName: "Suprise Event",
+        eventTimingsStart: "04:00 PM",
+        eventTimingsEnd: "05:00 PM",
+        eventLocation: "Rajam Hall"
+    },
+    {
+        eventName: "Dinner",
+        eventTimingsStart: "05:00 PM",
+        eventTimingsEnd: "06:30 PM",
+        eventLocation: ""
+    },
+    {
+        eventName: "Battle Of Bands Inter",
+        eventTimingsStart: "06:30 PM",
+        eventTimingsEnd: "08:00 PM",
+        eventLocation: "Open Air Theatre"
+    },
+    {
+        eventName: "Mega Dance (Host)",
+        eventTimingsStart: "08:00 PM",
+        eventTimingsEnd: "08:20 PM",
+        eventLocation: "Open Air Theatre"
+    },
+    {
+        eventName: "Fashion Show Intra",
+        eventTimingsStart: "08:20 PM",
+        eventTimingsEnd: "08:50 PM",
+        eventLocation: "Open Air Theatre"
+    },
+    {
+        eventName: "Celebrities & Concerts",
+        eventTimingsStart: "08:50 PM",
+        eventTimingsEnd: "10:00 PM",
+        eventLocation: "Open Air Theatre"
+    }
+]
+
+let publicEventsSaturday = [
+    {
+        eventName: "Stepless Step Intra",
+        eventTimingsStart: "10:00 AM",
+        eventTimingsEnd: "11:00 AM",
+        eventLocation: "Rajam Hall"
+    },
+    {
+        eventName: "Poutpouri Inter",
+        eventTimingsStart: "11:00 AM",
+        eventTimingsEnd: "12:00 PM",
+        eventLocation: "Rajam Hall"
+    },
+    {
+        eventName: "Solo Singing Intra",
+        eventTimingsStart: "12:00 PM",
+        eventTimingsEnd: "12:30 PM",
+        eventLocation: "Rajam Hall"
+    },
+    {
+        eventName: "Lunch",
+        eventTimingsStart: "12:30 PM",
+        eventTimingsEnd: "02:00 PM",
+        eventLocation: ""
+    },
+    {
+        eventName: "Adzap Inter",
+        eventTimingsStart: "02:00 PM",
+        eventTimingsEnd: "03:30 PM",
+        eventLocation: "Rajam Hall"
+    },
+    {
+        eventName: "Channel Surfing Inter",
+        eventTimingsStart: "03:30 PM",
+        eventTimingsEnd: "05:00 PM",
+        eventLocation: "Rajam Hall"
+    },
+    {
+        eventName: "Dinner",
+        eventTimingsStart: "05:00 PM",
+        eventTimingsEnd: "06:30 PM",
+        eventLocation: ""
+    },
+    {
+        eventName: "Tamil Mandram Intra",
+        eventTimingsStart: "06:30 PM",
+        eventTimingsEnd: "07:15 PM",
+        eventLocation: "Open Air Theatre"
+    },
+    {
+        eventName: "Variety Inter",
+        eventTimingsStart: "07:15 PM",
+        eventTimingsEnd: "09:00 PM",
+        eventLocation: "Open Air Theatre"
+    },
+    {
+        eventName: "Celebrities & Concerts",
+        eventTimingsStart: "09:00 PM",
+        eventTimingsEnd: "10:00 PM",
+        eventLocation: "Open Air Theatre"
+    }
+]
+
+let publicEventsSunday = [
+    {
+        eventName: "Nadanam (Tamil Mandram)",
+        eventTimingsStart: "08:30 AM",
+        eventTimingsEnd: "12:30 PM",
+        eventLocation: "Rajam Hall"
+    },
+    {
+        eventName: "Stepless Step Inter",
+        eventTimingsStart: "02:00 PM",
+        eventTimingsEnd: "03:00 PM",
+        eventLocation: "Rajam Hall"
+    },
+    {
+        eventName: "Solo Dance Inter",
+        eventTimingsStart: "03:00 PM",
+        eventTimingsEnd: "04:15 PM",
+        eventLocation: "Rajam Hall"
+    },
+    {
+        eventName: "Duo Dance Inter",
+        eventTimingsStart: "04:15 PM",
+        eventTimingsEnd: "05:15 PM",
+        eventLocation: "Rajam Hall"
+    },
+    {
+        eventName: "Adaptunes & Battle Of Dance (Inter)",
+        eventTimingsStart: "05:15 PM",
+        eventTimingsEnd: "06:30 PM",
+        eventLocation: "Rajam Hall"
+    },
+    {
+        eventName: "Dinner",
+        eventTimingsStart: "06:30 PM",
+        eventTimingsEnd: "07:00 PM",
+        eventLocation: ""
+    },
+    {
+        eventName: "Choreo Night",
+        eventTimingsStart: "07:00 PM",
+        eventTimingsEnd: "09:00 PM",
+        eventLocation: "Open Air Theatre"
+    },
+    {
+        eventName: "Celebrities & Concerts",
+        eventTimingsStart: "09:00 PM",
+        eventTimingsEnd: "10:00 PM",
+        eventLocation: "Open Air Theatre"
+    }
+]
